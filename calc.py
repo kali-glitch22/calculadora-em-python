81d499369b9c2f2802c9bcd5dde885806cff4973
kali-glitch <73629043+kali-glitch22@users.noreply.github.com>
2020-11-16 16:59:13 -0300

    calc 3.0

--- calc.py
+++ calc.py
@@ -3,6 +3,13 @@ from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 from os import system
 from platform import system as ss
+def stri(self):
+    h = str(self)
+    l = h.replace('[', '')
+    b = l.replace(']', '')
+    k = b.replace(',', '')
+    global j
+    j = k.replace("'", '')
 cac = 1
 if cac == 1:
     class calc:
@@ -31,78 +38,157 @@ if cac == 1:
         [ 12 ] FATORIAL
         [ 13 ] PRIMOS
         [ 14 ] LIMPAR
-        [ 15 ] SAIR
-        Aperte uma letra para sair do loop no meio das contas""")
+        [ 15 ] LOGARITIMO
+        [ 16 ] SAIR""")
+
 
             def soma():
+                global c
                 cs = 0
                 o = 1
+
+                a = list()
                 while True:
                     try:
                         n = float(input(f'digite seu {o} numero: '.capitalize()))
+                        if o == 1:
+                            a.insert(1, '+')
                     except:
+                        if a.pop() == '+' and o > 1:
+                            a.remove('+')
+                        a.append('=')
                         break
-                    cs += n
+
                     o += 1
-                return print(str(cs).rjust(10))
+                    cs += n
+                    a.append(n)
+                    c = len(a)
+                    if a[:c:2]:
+                        a.append('+')
+
+                stri(a)
+
+                return print(f'\n{j}'.center(12), (str(cs).rjust(1)))
 
             def menos():
                 cs = 0
                 o = 1
+                global c
+                global n
+                global n1
+                global a
+                a = list()
                 while True:
                     if o == 1:
                         try:
                             global n1
                             n1 = float(input(f'digite seu 1 numero: '.capitalize()))
+                            a.append(n1)
+                            a.insert(1, '-')
                         except:
+
+                            a.append('=')
                             break
                     elif o != 1:
                         try:
                             n = float(input(f'digite seu {o} numero: '.capitalize()))
+                            a.append(n)
                         except:
+                            if a.pop() == '-' and o > 1:
+                                a.remove('-')
+                            a.append('=')
                             break
+
+                    c = len(a)
+                    if a[:c:2]:
+                        a.append('-')
                     k = n1
                     if o == 2:
                         cs = k - n
                     if o >= 3:
                         cs -= n
                     o += 1
-                return print(str(cs).rjust(10))
+
+                stri(a)
+
+                return print(f'\n{j}'.center(12), (str(cs).rjust(1)))
 
             def mutiplica():
+                global c
+                cs = 0
+                a = list()
                 cx = 1
                 o = 1
                 while True:
-                    try:
-                        n = float(input(f'digite seu {o} numero: '.capitalize()))
-                    except:
-                        break
-                    cx *= n
-                    o += 1
-                return print(str(cx).rjust(10))
+                    global c
+                    cs = 1
+                    o = 1
+
+                    a = list()
+                    while True:
+                        try:
+                            n = float(input(f'digite seu {o} numero: '.capitalize()))
+                            if o == 1:
+                                a.insert(1, 'x')
+                        except:
+                            if a.pop() == 'x' and o > 1:
+                                a.remove('x')
+                            a.append('=')
+                            break
+
+                        o += 1
+                        cs *= n
+                        a.append(n)
+                        c = len(a)
+                        if a[:c:2]:
+                            a.append('x')
+
+                    stri(a)
+
+                    return print(f'\n{j}'.center(12), (str(cs).rjust(1)))
 
             def fracao():
-                cx = 1
+                cs = 1
                 o = 1
+                global c
+                global n
+                global n1
+                global a
+                a = list()
                 while True:
                     if o == 1:
                         try:
                             global n1
                             n1 = float(input(f'digite seu 1 numero: '.capitalize()))
+                            a.append(n1)
+                            a.insert(1, '÷')
                         except:
+
+                            a.append('=')
                             break
                     elif o != 1:
                         try:
                             n = float(input(f'digite seu {o} numero: '.capitalize()))
+                            a.append(n)
                         except:
+                            if a.pop() == '÷' and o > 1:
+                                a.remove('÷')
+                            a.append('=')
                             break
+
+                    c = len(a)
+                    if a[:c:2]:
+                        a.append('÷')
                     k = n1
                     if o == 2:
                         cs = k / n
                     if o >= 3:
                         cs /= n
                     o += 1
-                print(str(cs).rjust(10))
+
+                stri(a)
+
+                return print(f'\n{j}'.center(12), (str(cs).rjust(1)))
 
             def expo():
                 try:
@@ -111,24 +197,27 @@ if cac == 1:
                 except:
                     print('*-*')
                 cx = o ** v
-                return print(str(cx).rjust(10))
+                return print('\n', f'{o} ^ {v} ='.center(12), str(cx).rjust(1))
 
             def raiz():
+                global o, v
                 try:
-                    o = float(input('digite seu numero: '))
+                    o = float(input('digite seu numero: '.capitalize()))
                     v = float((input('digite sua raiz: '.capitalize())))
                 except:
                     print('*-*')
                 cx = o ** (1 / v)
-                print(cx)
+                return print('\n', f'√{o} ='.center(12), str(cx).rjust(1))
+
 
             def sen():
+                global o
                 try:
                     o = float(input('digite seu numero: '.capitalize()))
                 except:
                     print('*-*')
                 cx = math.sin(o)
-                print(cx)
+                return print('\n', f'sen({o}) ='.center(12), str(cx).rjust(1))
 
             def cos():
                 try:
@@ -136,7 +225,7 @@ if cac == 1:
                 except:
                     print('*-*')
                 cx = math.cos(o)
-                print(cx)
+                return print('\n', f'cos({o}) ='.center(12), str(cx).rjust(1))
 
             def tan():
                 try:
@@ -144,7 +233,7 @@ if cac == 1:
                 except:
                     print('*-*')
                 cx = math.tan(o)
-                print(cx)
+                return print('\n', f'tan({o}) ='.center(12), str(cx).rjust(1))
 
             def arccos():
 
@@ -153,7 +242,7 @@ if cac == 1:
                 except:
                     print('*-*')
                 cx = math.cos(o) ** -1
-                print(cx)
+                return print('\n', f'arcsen({o}) ='.center(12), str(cx).rjust(1))
 
             def arcsen():
 
@@ -162,24 +251,30 @@ if cac == 1:
                 except:
                     print('*-*')
                 cx = math.sin(o) ** -1
-                print(cx)
+                return print('\n', f'arccos({o}) ='.center(12), str(cx).rjust(1))
 
             def arctan():
 
                 try:
-                    o = float(input('digite seu numero: '))
+                    o = float(input('digite seu numero: '.capitalize()))
                 except:
                     print('*-*')
                 cx = math.tan(o) ** -1
-                print(cx)
+                return print('\n', f'arctan({o}) ='.center(12), str(cx).rjust(1))
 
             def fact():
-                n = int(input('digite um numero: '.capitalize()))
+               n = int(input('digite um numero: '.capitalize()))
                 cx = math.factorial(n)
-                print(cx)
+                print(f'{n}! = ', end='')
+                for l in range(1, n + 1, 1):
+                    if l == n:
+                        print(f'{l}', f'= {cx}'.rjust(1))
+                        break
+                    print(f'{l}', 'x ', end='')
+                return
             def primos():
-                n = int(input('digite um numero inicial: '))
-                n1 = int(input('digite um numero final: '))
+                n = int(input('digite um numero inicial: '.capitalize()))
+                n1 = int(input('digite um numero final: '.capitalize()))
                 for c in range(n, n1 + 1):
                         if c != 1:
                             if c % 2 != 0 and c % 3 != 0 and c % 4 != 0 and c % 5 != 0 and c % 6 != 0 and c % 7 != 0 and c % 8 != 0 and c % 9 != 0 or c == 2 or c == 3 or c == 5 or c == 7:
@@ -190,7 +285,7 @@ if cac == 1:
                     system("cls")
                 if ss() == 'Linux':
                     system("clear")
-                return print("""    [ 0 ] ADIÇÃO 
+                return print("""    [ 0 ] ADIÇÃO
     [ 1 ] SUBTRAÇÃO 
     [ 2 ] MUTIPLICAÇÃO
     [ 3 ] DIVISÃO 
@@ -205,11 +300,16 @@ if cac == 1:
     [ 12 ] FATORIAL
     [ 13 ] PRIMOS
     [ 14 ] LIMPAR
-    [ 15 ] SAIR""")
-
+    [ 15 ] LOGARITIMO
+    [ 16 ] SAIR""")
+            def log():
+                n = float(input('digite o numero: '.capitalize()))
+                b = int(input('digite a base: '.capitalize()))
+                result = math.log(n, b)
+                return print('\n', f'log({n}) ='.center(12), str(result).rjust(1))
             while True:
                 r = int(input("opção: ".capitalize()))
-                if r >= 15:
+                if r >= 16:
                     break
                 if r == 0:
                     soma()
@@ -241,6 +341,8 @@ if cac == 1:
                     primos()
                 if r == 14:
                     limpar()
+                if r == 15:
+                    log()
         except:
             print('algo deu errado')
 
